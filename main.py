880f7a48fbf7d407c2a57ac1327875726634cc22
Yassir <yassirdriuch@gmail.com>
2026-05-19 15:45:56 +0200

    Added weather forecast lookup

--- main.py
+++ main.py
@@ -18,32 +18,27 @@ WEATHER_CODES = {
     51: "Lichte motregen",
     53: "Matige motregen",
     55: "Dichte motregen",
-
     56: "Lichte ijzelmotregen",
     57: "Dichte ijzelmotregen",
 
     61: "Lichte regen",
     63: "Matige regen",
     65: "Zware regen",
-
     66: "Lichte ijzelregen",
     67: "Zware ijzelregen",
 
     71: "Lichte sneeuwval",
     73: "Matige sneeuwval",
     75: "Zware sneeuwval",
-
     77: "Sneeuwkorrels",
 
     80: "Lichte regenbuien",
     81: "Matige regenbuien",
     82: "Hevige regenbuien",
-
     85: "Lichte sneeuwbuien",
     86: "Zware sneeuwbuien",
 
     95: "Onweer",
-
     96: "Onweer met lichte hagel",
     99: "Onweer met zware hagel",
 }
